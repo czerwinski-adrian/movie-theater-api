c86a9fe0a05de2b74ef6a57cc479bccbe92197e9
czerwinski-adrian <adrianczerwinski3@gmail.com>
2023-02-25 13:39:53 +0000

    get shows/show added, error still occurs

--- routes/shows.js
+++ routes/shows.js
@@ -4,7 +4,28 @@ const { Router } = require("express");
 const showsRouter = Router();
 
 //Get all shows
+usersRouter.get('/shows', async (req, res) => {
+    try {
+        const allShows = await Show.findAll();
+        res.status(200).send(allShows);
+    } catch (error) {
+        res.status(500).send({ err: error.message });
+    }
+});
 
+//Get single show
+usersRouter.get('/shows/:id', async (req, res) => {
+    try {
+        const user = await Show.findByPk(req.params.id);
+        if (!Show){
+            res.status(404).send({ err: 'Show not found' });
+        }else{
+            res.status(200).send(user);
+        }
+    } catch (error) {
+        res.status(500).send({ err: error.message });
+    }
+});
 
 //Export
 module.exports = showsRouter
